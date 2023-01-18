5a5d08f8eb9d87b371f3306c3f5f09ee4b2654e7
t.vladimir <vovatatosyan2000@mail.ru>
2023-01-18 15:04:27 +0300

    Added sub templates for post

--- app/routes.py
+++ app/routes.py
@@ -67,34 +67,3 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
